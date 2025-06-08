fe3743271d59dbae4a12149b250c3a087f6cc43a
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-08 19:32:26 +0530

    Create samplecode.cpp

---
+++ samplecode.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //int a,b;
+    /*float a,b;
+    cout<<"Enter integer value:"<<endl;
+    cin>>a>>b;
+    cout<<"Total:"<<a+b;
+    char a;
+    cout<<"\n ENter the character : ";
+    cin>>a;
+    cout<<"Character is :"<<a;
+    string a;
+    cout<<"\n Enter String :";
+    cin>>a;
+    cout<<a;*/
+    string a;
+    cout<<"\n Enter a para :";
+    getline(cin,a);
+    cout<<a;
+    return 0;
+}
