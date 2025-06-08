8522b547f772520d476651be82ad3deb27598d69
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-08 22:27:27 +0530

    Create if-else.cpp

---
+++ if-else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"\n Enter the value of A & B :";
+    cin>>a>>b;
+    if(a>b){
+        cout<<"A  is gratest no "<<endl;
+    }
+    if(a<b){
+        cout<<"B  is gratest no "<<endl;
+    }
+    else{
+        cout<<"Both are same "<<endl;
+    }
+    return 0;
+}     
