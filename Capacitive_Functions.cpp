56fd2d66313b7fdf71d09bd5037351703c525fcc
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-08 21:54:23 +0530

    Create Capacitive_Functions.cpp

---
+++ Capacitive_Functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* 
+c++ string
+    Input Function
+    Capacity Function
+    Iterator Function
+    Manipulating Function  */
+    
+int main()
+{
+    // Capacitive Functions
+    string str("kavinmayil");  // class name   object name("constructor value")
+    cout<<str<<endl;
+    cout<<"Size :"<<str.size()<<endl;
+    cout<<"length :"<<str.length()<<endl;
+    cout<<"Max Size :"<<str.max_size()<<endl;
+    return 0;
+}     
