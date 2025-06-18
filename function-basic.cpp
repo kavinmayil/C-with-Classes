d2ae43e8732426f5e01e2bf78572ae37953c621c
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-18 16:57:10 +0530

    Create function-basic.cpp

---
+++ function-basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void display(){
+    int a,b,c;
+    cout<<"Enter 2 Nos :"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Total :"<<c<<endl;
+}
+
+int main()
+{
+    display();
+    display();
+    display();
+    return 0;
+}
