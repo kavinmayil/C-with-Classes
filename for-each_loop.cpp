a54c6733d169a85222de092a38f1b7aba5047787
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-18 12:51:41 +0530

    Create for-each_loop.cpp

---
+++ for-each_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a[]={65,66,67,68,69,70};
+    for(char x :a)   //auto for all data type
+    {
+        cout<<x<<endl;
+    }
+    return 0;
+}
