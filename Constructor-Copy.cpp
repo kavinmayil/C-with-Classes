efcb771941602def4d5953033ad1f71c6d1aa758
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-19 19:36:19 +0530

    Create Constructor-Copy.cpp

---
+++ Constructor-Copy.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class math{
+    private:
+      int a,b,c;
+    public:
+      math(int x,int y){
+          a=x;
+          b=y;
+      }
+      math(math &x){
+          a=x.a;
+          b=x.b;
+      }
+      void add(){
+          c=a+b;
+          cout<<"Total :"<<c<<endl;
+      }
+};
+
+int main(){
+    math o(10,25);
+    math o1(o);
+    o.add();
+    o1.add();
+    return 0;
+}
