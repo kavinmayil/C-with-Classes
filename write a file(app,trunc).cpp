bdfc9ea52857bc24e3def388d0cd4b7ca4128d83
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-06-30 08:51:03 +0530

    Create write a file(app,trunc).cpp

---
+++ write a file(app,trunc).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ofstream o("test.txt",ios::trunc);//ios::app
+    o<<"Kavinmayil...."<<endl;
+    o<<"711222106027.."<<endl;
+    o<<"ECE..........."<<endl;
+    o<<"12333333"<<endl;
+    return 0;
+}`
